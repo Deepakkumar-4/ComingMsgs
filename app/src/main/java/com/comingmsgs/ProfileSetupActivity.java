716b722b7a78943af509a5dde6f2fab35a73e0c2
Deepakkumar-4 <godotdownload@gmail.com>
2021-12-26 23:36:02 +0530

    profile setup activity code done but it loads main activity 4 times

--- app/src/main/java/com/comingmsgs/ProfileSetupActivity.java
+++ app/src/main/java/com/comingmsgs/ProfileSetupActivity.java
@@ -1,16 +1,23 @@
 package com.comingmsgs;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import com.comingmsgs.databinding.ActivityProfileSetupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class ProfileSetupActivity extends AppCompatActivity {
     ActivityProfileSetupBinding binding;
@@ -18,6 +25,7 @@ public class ProfileSetupActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseDatabase database;
     FirebaseStorage storage;
+    Uri selectedImage;
 
 
 
@@ -42,6 +50,65 @@ public class ProfileSetupActivity extends AppCompatActivity {
                 startActivityForResult(intent,45);
             }
         });
+        binding.setupProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = binding.enterName.getText().toString();
+
+                if (name.isEmpty()) {
+                    binding.enterName.setError("Please enter your name");
+                    return;
+                }
+
+                if (selectedImage != null)
+                {
+                    StorageReference reference = storage.getReference().child("Profiles").child(auth.getUid());
+                    reference.putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if(task.isSuccessful()){
+                                reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri)
+                                    //here uri in small letters is url for profile image
+                                    {
+                                        String imageUrl = uri.toString();
+                                        String uid = auth.getUid();
+                                        String phone  = auth.getCurrentUser().getPhoneNumber();
+                                        String name = binding.enterName.getText().toString();
+
+                                        User user =  new User(uid ,name,phone,imageUrl);
+                                        // here we get all data that user entered while his login
+
+                                        // now time to put it in firebase
+
+                                        database.getReference()
+                                                .child("Users")
+                                                .setValue(user)//if these items get uploaded succesfuly then intent will  run
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void unused) {
+                                                        Intent  intent = new Intent(ProfileSetupActivity.this , MainActivity.class);
+                                                        startActivity(intent);
+                                                        finishAffinity();
+
+
+                                                    }
+                                                });
+
+
+
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+
+
+            }
+        });
+
     }
 
     @Override
@@ -52,6 +119,7 @@ public class ProfileSetupActivity extends AppCompatActivity {
         if (data != null){
             if (data.getData() !=null){
                 binding.profilepic.setImageURI(data.getData());
+                selectedImage = data.getData();
             }
 
         }
